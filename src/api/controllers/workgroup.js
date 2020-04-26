04307402e699d7672cd2701efc427f38f6600e7e
Matteo <orsini.matteo99@gmail.com>
2020-04-26 16:21:31 +0200

    Added workgroup's endpoints

--- src/api/controllers/workgroup.js
+++ src/api/controllers/workgroup.js
@@ -5,17 +5,85 @@ const workgroupService = require("../services/workgroup");
 
 // Get all the workgroups
 router.get("/", async (req, res) => {
-	const userId = 1;
-	const workgroups = await workgroupService.getAllWorkgroups(userId);
-	res.json({ data: workgroups });
+	try {
+		const userId = 1;
+		const workgroups = await workgroupService.getAllWorkgroups(userId);
+		res.json({ data: workgroups });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Create workgroup
+router.post("/", async (req, res) => {
+	try {
+		const userId = 1;
+		const { name, image } = req.body;
+		const workgroup = await workgroupService.createWorkgroup(userId, name, image);
+		res.json({ data: workgroup });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
 });
 
 // Get specific workgroup
 router.get("/:idWorkgroup", async (req, res) => {
-	const { idWorkgroup } = req.params;
-	const userId = 1;
-	const workgroup = await workgroupService.getWorkgroup(userId, idWorkgroup);
-	res.json({ data: workgroup });
+	try {
+		const { idWorkgroup } = req.params;
+		const userId = 1;
+		const workgroup = await workgroupService.getWorkgroup(userId, idWorkgroup);
+		res.json({ data: workgroup });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Delete workgroup
+router.delete("/:idWorkgroup", async (req, res) => {
+	try {
+		const { idWorkgroup } = req.params;
+		const userId = 1;
+		const workgroup = await workgroupService.deleteWorkgroup(userId, idWorkgroup);
+		res.json({ data: workgroup });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Add member to workgroup
+router.put("/:idWorkgroup/member/:idMember", async (req, res) => {
+	try {
+		const { idWorkgroup, idMember } = req.params;
+		const userId = 1;
+		await workgroupService.addMember(userId, idWorkgroup, idMember);
+		res.sendStatus(200);
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Delete member from workgroup
+router.delete("/:idWorkgroup/member/:idMember", async (req, res) => {
+	try {
+		const { idWorkgroup, idMember } = req.params;
+		const userId = 1;
+		await workgroupService.removeMember(userId, idWorkgroup, idMember);
+		res.sendStatus(200);
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Get all members of a workgroup
+router.get("/:idWorkgroup/member", async (req, res) => {
+	try {
+		const { idWorkgroup } = req.params;
+		const userId = 1;
+		const members = await workgroupService.getAllMembers(userId, idWorkgroup);
+		res.json({ data: members });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
 });
 
 const drive = require("./drive");
--- src/api/services/workgroup.js
+++ src/api/services/workgroup.js
@@ -13,5 +13,49 @@ exports.getWorkgroup = async (userId, workgroupId) => {
 		[userId, workgroupId]
 	);
 	if (results.rowCount == 0) throw new Error("Il workgroup con id " + workgroupId + " non esiste oppure non è associato all'utente " + userId);
+	return results.rows[0];
+};
+
+exports.createWorkgroup = async (userId, name, image) => {
+	const client = await pool.connect();
+	const result = await client.query('INSERT INTO "WorkGroup"(name, image, owner) VALUES ($1, $2, $3) RETURNING *', [name, image, userId]);
+	if (result.rowCount == 1) {
+		const workgroupId = result.rows[0].id;
+		await client.query('INSERT INTO "UserWorkGroup" VALUES ($1, $2)', [userId, workgroupId]);
+		return result.rows[0];
+	}
+	client.release();
+	throw new Error("Creazione del workgroup fallita");
+};
+
+exports.deleteWorkgroup = async (userId, workgroupId) => {
+	const client = await pool.connect();
+	const workgroup = await this.getWorkgroup(userId, workgroupId);
+	if (workgroup.owner !== userId) throw new Error("Solo il proprietario del workgroup può eliminarlo");
+	await client.query('DELETE FROM "UserWorkGroup" WHERE userid = $1 AND workgroup = $2', [userId, workgroupId]);
+	const results = await client.query('DELETE FROM "WorkGroup" WHERE id = $1 RETURNING *', [workgroupId]);
+	client.release();
 	return results.rows;
+};
+
+exports.addMember = async (userId, workgroupId, memberId) => {
+	const workgroup = await this.getWorkgroup(userId, workgroupId);
+	if (workgroup.owner !== userId) throw new Error("Solo il proprietario del workgroup può aggiungere un membro");
+	await pool.query('INSERT INTO "UserWorkGroup" VALUES ($1, $2)', [memberId, workgroupId]);
+};
+
+exports.removeMember = async (userId, workgroupId, memberId) => {
+	const workgroup = await this.getWorkgroup(userId, workgroupId);
+	if (workgroup.owner !== userId) throw new Error("Solo il proprietario del workgroup può aggiungere un membro");
+	await pool.query('DELETE FROM "UserWorkGroup" WHERE userid = $1 AND workgroup = $2', [memberId, workgroupId]);
+};
+
+exports.getAllMembers = async (userId, workgroupId) => {
+	const result = await pool.query(
+		'SELECT u.id, u.email, u.firstname, u.lastname FROM "UserWorkGroup" uwg, "User" u WHERE uwg.userid = u.id AND uwg.workgroup = $1',
+		[workgroupId]
+	);
+	const userFound = result.rows.find((user) => user.id === userId);
+	if (!userFound) throw new Error("L'utente " + userId + " non fa parte dei membri del workgroup");
+	return result.rows;
 };
