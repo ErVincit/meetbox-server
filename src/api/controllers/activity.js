d0305e431c80e125955f832566c4df03cf6dc6d0
Matteo <orsini.matteo99@gmail.com>
2020-04-26 23:16:31 +0200

    Added CreateTask, DeleteTask and GetAllTasks

--- src/api/controllers/activity.js
+++ src/api/controllers/activity.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 
 const activityService = require("../services/activity");
 
+// Create section
 router.post("/section", async (req, res) => {
 	try {
 		const { idWorkgroup } = req.params;
@@ -15,6 +16,7 @@ router.post("/section", async (req, res) => {
 	}
 });
 
+// Delete section
 router.delete("/section/:idSection", async (req, res) => {
 	try {
 		const { idWorkgroup, idSection } = req.params;
@@ -25,6 +27,7 @@ router.delete("/section/:idSection", async (req, res) => {
 	}
 });
 
+// Change section title
 router.put("/section/:idSection", async (req, res) => {
 	try {
 		const { idWorkgroup, idSection } = req.params;
@@ -36,11 +39,46 @@ router.put("/section/:idSection", async (req, res) => {
 	}
 });
 
+// Get all sections
 router.get("/section", async (req, res) => {
 	try {
 		const { idWorkgroup } = req.params;
 		const sections = await activityService.getAllSections(idWorkgroup, req.currentUser);
 		res.json({ data: sections });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Create task
+router.post("/section/:idSection/task", async (req, res) => {
+	try {
+		const { idWorkgroup, idSection } = req.params;
+		const { title, description } = req.body;
+		const task = await activityService.createTask(idSection, idWorkgroup, req.currentUser, title, description);
+		res.json({ data: task });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Delete task
+router.delete("/section/:idSection/task/:idTask", async (req, res) => {
+	try {
+		const { idWorkgroup, idSection, idTask } = req.params;
+		const task = await activityService.deleteTask(idTask, idSection, idWorkgroup, req.currentUser);
+		res.json({ data: task });
+	} catch (err) {
+		res.json({ error: err.name, message: err.message });
+	}
+});
+
+// Get all the tasks
+router.get("/section/:idSection/task", async (req, res) => {
+	try {
+		const { idWorkgroup, idSection } = req.params;
+		const tasks = await activityService.getAllTasks(idSection, idWorkgroup, req.currentUser);
+		res.json({ data: tasks });
 	} catch (err) {
 		res.json({ error: err.name, message: err.message });
 	}
--- src/api/services/activity.js
+++ src/api/services/activity.js
@@ -1,43 +1,49 @@
 const pool = require("../../database");
 
 exports.createSection = async (title, workgroupId, userId) => {
+	const client = await pool.connect();
 	// Check if user is a member of the workgroup
-	const exists = await pool.query(
+	const exists = await client.query(
 		'SELECT wg.* FROM "UserWorkGroup" uwg, "WorkGroup" wg WHERE uwg.userid = $1 AND uwg.workgroup = wg.id AND wg.id = $2',
 		[userId, workgroupId]
 	);
 	if (exists.rowCount == 0) throw new Error("Non è possibile creare una sezione in un workgroup di cui non fai parte");
 	// Create section
-	const results = await pool.query('INSERT INTO "Section" (title, workgroup) VALUES ($1, $2) RETURNING *', [title, workgroupId]);
+	const results = await client.query('INSERT INTO "Section" (title, workgroup) VALUES ($1, $2) RETURNING *', [title, workgroupId]);
+	client.release();
 	return results.rows[0];
 };
 
 exports.deleteSection = async (sectionId, workgroupId, userId) => {
+	const client = await pool.connect();
 	// Check if user is a member of the workgroup
-	const exists = await pool.query(
+	const exists = await client.query(
 		'SELECT wg.* FROM "UserWorkGroup" uwg, "WorkGroup" wg WHERE uwg.userid = $1 AND uwg.workgroup = wg.id AND wg.id = $2',
 		[userId, workgroupId]
 	);
 	if (exists.rowCount == 0) throw new Error("Non è possibile eliminare una sezione in un workgroup di cui non fai parte");
 	// Delete all task of that section
-	const results = await pool.query('DELETE FROM "Task" WHERE section = $1', [sectionId]);
+	const results = await client.query('DELETE FROM "Task" WHERE section = $1', [sectionId]);
 	// Delete the section
-	await pool.query('DELETE FROM "Section" WHERE id = $1', [sectionId]);
+	await client.query('DELETE FROM "Section" WHERE id = $1', [sectionId]);
+	client.release();
 	return results.rows;
 };
 
 exports.changeSectionTitle = async (newTitle, sectionId, workgroupId, userId) => {
+	const client = await pool.connect();
 	// Check preconditions
-	const exists = await pool.query(
+	const exists = await client.query(
 		'SELECT * FROM "UserWorkGroup" uwg, "WorkGroup" wg, "Section" s WHERE uwg.userid = $1 AND uwg.workgroup = wg.id AND wg.id = $2 AND s.workgroup = wg.id AND s.id = $3',
 		[userId, workgroupId, sectionId]
 	);
 	if (exists.rowCount == 0)
 		throw new Error(
-			"Operazione fallita. Potresti aver richiesto di modificare un gruppo di lavoro o sezione insesistente oppure di risorse di cui non hai accesso"
+			"Operazione fallita. Potresti aver richiesto di modificare un gruppo di lavoro o sezione inesistente oppure di risorse di cui non hai l'accesso"
 		);
 	// Change title
-	const result = await pool.query('UPDATE "Section" SET title = $1 WHERE id = $2 RETURNING *', [newTitle, sectionId]);
+	const result = await client.query('UPDATE "Section" SET title = $1 WHERE id = $2 RETURNING *', [newTitle, sectionId]);
+	client.release();
 	return result.rows[0];
 };
 
@@ -46,6 +52,54 @@ exports.getAllSections = async (workgroupId, userId) => {
 	const results = await pool.query(
 		'SELECT s.* FROM "UserWorkGroup" uwg, "WorkGroup" wg, "Section" s WHERE uwg.userid = $1 AND uwg.workgroup = wg.id AND wg.id = $2 AND s.workgroup = wg.id',
 		[userId, workgroupId]
+	);
+	return results.rows;
+};
+
+exports.createTask = async (sectionId, workgroupId, userId, title, description) => {
+	const client = await pool.connect();
+	// Check preconditions
+	const exists = await client.query(
+		'SELECT * FROM "UserWorkGroup" uwg, "WorkGroup" wg, "Section" s WHERE uwg.userid = $1 AND uwg.workgroup = wg.id AND wg.id = $2 AND s.workgroup = wg.id AND s.id = $3',
+		[userId, workgroupId, sectionId]
+	);
+	if (exists.rowCount == 0)
+		throw new Error(
+			"Operazione fallita. Potresti aver richiesto di inserire un gruppo di lavoro o sezione inesistente oppure di risorse di cui non hai l'accesso"
+		);
+	// Create the task
+	const results = await client.query(
+		'INSERT INTO "Task" (title, description, section, owner) VALUES ($1, $2, $3, $4) RETURNING id, title, description',
+		[title, description, sectionId, userId]
+	);
+	client.release();
+	return results.rows[0];
+};
+
+exports.deleteTask = async (taskId, sectionId, workgroupId, userId) => {
+	const client = await pool.connect();
+	// Check preconditions
+	const exists = await client.query(
+		`SELECT *
+		FROM "UserWorkGroup" uwg, "WorkGroup" wg, "Section" s, "Task" t
+		WHERE uwg.userid = $1 AND uwg.workgroup = wg.id AND wg.id = $2 AND s.workgroup = wg.id AND s.id = $3 AND t.section = s.id AND t.id = $4`,
+		[userId, workgroupId, sectionId, taskId]
+	);
+	if (exists.rowCount == 0)
+		throw new Error(
+			"Operazione fallita. Potresti aver richiesto di inserire un gruppo di lavoro, sezione e/o task inesistente oppure di risorse di cui non hai l'accesso"
+		);
+	// Delete the task
+	const results = await client.query('DELETE FROM "Task" WHERE id = $1 AND section = $2 RETURNING id, title, description', [taskId, sectionId]);
+	client.release();
+	return results.rows[0];
+};
+
+exports.getAllTasks = async (sectionId, workgroupId, userId) => {
+	// Get all the tasks of the user
+	const results = await pool.query(
+		'SELECT t.* FROM "UserWorkGroup" uwg, "WorkGroup" wg, "Section" s, "Task" t WHERE uwg.userid = $1 AND uwg.workgroup = wg.id AND wg.id = $2 AND s.workgroup = wg.id AND t.section = s.id AND s.id = $3',
+		[userId, workgroupId, sectionId]
 	);
 	return results.rows;
 };
