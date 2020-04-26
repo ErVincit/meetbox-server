b4c584537948a657d98953efd6a27410bad363e0
Emanuele <emanuelevincitorio@hotmail.it>
2020-04-26 17:44:03 +0200

    Implemented hashing

--- src/api/services/login.js
+++ src/api/services/login.js
@@ -1,4 +1,5 @@
 const pool = require("../../database");
+const crypto = require('crypto');
 
 exports.login = async (username, password) => {
 	const sqlUsernameExists = 'SELECT * FROM "User" u WHERE u.email = $1';
@@ -7,7 +8,7 @@ exports.login = async (username, password) => {
 		// Confrontare la password dell'utente tramite SQL
 		const id = res.rows[0].id;
 		const resPassword = await pool.query('SELECT uc.password FROM "UserCredential" uc WHERE uc.userid = $1;', [id]);
-		if (resPassword.rowCount > 0 && resPassword.rows[0].password === password) return id;
+		if (resPassword.rowCount > 0 && resPassword.rows[0].password === this.hashing(password)) return id;
 	}
 	throw new Error("Email e/o password sono errati"); 
 };
@@ -19,8 +20,7 @@ exports.registration = async (email, firstName, lastName, password) => {
 		const res = await pool.query('INSERT INTO "User"(email, firstName, lastName) VALUES ($1, $2, $3) RETURNING *;', [email, firstName, lastName]);
 		if (res.rowCount > 0) {
 			const id = res.rows[0].id;
-			// Hashare la password
-			await pool.query('INSERT INTO "UserCredential"(userid, password) VALUES ($1, $2) RETURNING *;', [id, password]);
+			await pool.query('INSERT INTO "UserCredential"(userid, password) VALUES ($1, $2) RETURNING *;', [id, this.hashing(password)]);
 			return id;
 		}
 		throw new Error("Registazione fallita. Controlla i dati e riprova");
@@ -29,3 +29,9 @@ exports.registration = async (email, firstName, lastName, password) => {
 		throw new Error("Registazione fallita. Controlla i dati e riprova");
 	}
 };
+
+exports.hashing = (password) => {
+	const hash = crypto.createHash('sha512');
+	hash.update(password);
+	return hash.digest('hex');
+}
