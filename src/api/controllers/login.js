8d960f32f45921470c811a2035546a4286aedbc8
alex6071 <alex.onofri98@gmail.com>
2020-05-04 17:10:35 +0200

    Modified Login Controller

--- src/api/controllers/login.js
+++ src/api/controllers/login.js
@@ -5,34 +5,45 @@ const jwt = require("jsonwebtoken");
 const loginService = require("../services/login");
 
 router.post("/", async (req, res, next) => {
-	const token = req.session.token;
-	if (token) return res.status(200).send({msg: 'Sei gia stato loggato. In un implementazione futura sarai ridirezionato alla homepage.'}); // Already logged
-	const { email, password } = req.body;
-	try {
-		const user = await loginService.login(email, password);
-		const iduser = user.id;
-		req.session.token = jwt.sign({iduser}, process.env.JWT_SECRET); //Da aggiungere per farlo scadere -> , {expiresIn: 60*x} -> dove x sono i minuti
-		return res.status(200).send({data: user});
-	} catch (err) {
-		return res.sendStatus(401);
-	}
+  const token = req.session.token;
+  if (token)
+    return res.status(200).send({
+      msg:
+        "Sei gia stato loggato. In un implementazione futura sarai ridirezionato alla homepage.",
+    }); // Already logged
+  const { email, password } = req.body;
+  try {
+    const user = await loginService.login(email, password);
+    const iduser = user.id;
+    req.session.token = jwt.sign({ iduser }, process.env.JWT_SECRET); //Da aggiungere per farlo scadere -> , {expiresIn: 60*x} -> dove x sono i minuti
+    return res.status(200).send({ data: user });
+  } catch (err) {
+    return res.sendStatus(401);
+  }
 });
 
 router.get("/validate", async (req, res, next) => {
-	const token = req.session.token;
-	if (token) return res.sendStatus(200);
-	return res.sendStatus(401);
+  const token = req.session.token;
+  if (token) return res.sendStatus(200);
+  return res.sendStatus(401);
 });
 
 router.post("/registration", async (req, res) => {
-	const { email, firstName, lastName, password } = req.body;
-	if (email || firstName || lastName || password) res.status(418);
-	try {
-		const result = await loginService.registration(email, firstName, lastName, password);
-		res.send({ data: result }).status(200);
-	} catch (err) {
-		res.status(412).send({ error: "RegistrationError", message: err });
-	}
+  const { email, firstName, lastName, password } = req.body;
+  if (!email || !firstName || !lastName || !password) return res.status(418);
+  try {
+    const result = await loginService.registration(
+      email,
+      firstName,
+      lastName,
+      password
+    );
+    return res.send({ data: result }).status(200);
+  } catch (err) {
+    return res
+      .status(412)
+      .send({ error: "RegistrationError", message: err.message });
+  }
 });
 
 module.exports = router;
