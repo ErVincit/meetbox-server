52fa61aa44598329fca7117303caf3f68e84686b
Matteo <orsini.matteo99@gmail.com>
2020-05-18 17:15:18 +0200

    Changed DB

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({ connectionString: process.env.DATABASE_ALTERNATIVO, ssl: false });
+const pool = new Pool({ connectionString: process.env.OFFICIAL_DB, ssl: true });
 pool.on("error", (error) => console.error("Error:", error));
 // NOTE: pg-cursor per leggere grandi quantità di informazioni
 
--- src/index.js
+++ src/index.js
@@ -16,61 +16,244 @@ main.use(
 	})
 );
 
+// main.get("/creaDB", async (req, res) => {
+// 	const pool = require("./database");
+// 	const client = await pool.connect();
+// 	let query;
+// 	query = `CREATE TABLE public."User"
+// 	(
+// 	    id serial NOT NULL,
+// 	    email character varying(500) NOT NULL,
+// 	    firstname character varying(100) NOT NULL,
+// 	    lastname character varying(100) NOT NULL,
+// 	    PRIMARY KEY (id),
+// 		UNIQUE(email)
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."UserCredential"
+// 	(
+// 	    userid integer NOT NULL,
+// 	    password character varying(256) NOT NULL,
+// 	    PRIMARY KEY (userid),
+// 	    FOREIGN KEY (userid)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	)`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."WorkGroup"
+// 	(
+// 	    id serial NOT NULL,
+// 	    name character varying(100) NOT NULL,
+// 	    image character varying(500),
+// 	    owner integer NOT NULL,
+// 	    PRIMARY KEY (id),
+// 	    FOREIGN KEY (owner)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."Section"
+// 	(
+// 	    id serial NOT NULL,
+// 	    title character varying(100) NOT NULL,
+// 	    workgroup integer NOT NULL,
+// 	    index integer NOT NULL,
+// 	    PRIMARY KEY (id),
+// 	    FOREIGN KEY (workgroup)
+// 	        REFERENCES public."WorkGroup" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."Label"
+// 	(
+// 	    id serial NOT NULL,
+// 	    name character varying(100),
+// 	    color character varying(6) NOT NULL,
+// 	    workgroup integer,
+// 	    PRIMARY KEY (id),
+// 	    FOREIGN KEY (workgroup)
+// 	        REFERENCES public."WorkGroup" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."Task"
+// 	(
+// 	    id serial NOT NULL,
+// 	    title character varying(100) NOT NULL,
+// 	    description character varying(1000),
+// 	    section integer NOT NULL,
+// 	    label integer NOT NULL,
+// 	    deadline timestamp with time zone,
+// 	    owner integer NOT NULL,
+// 	    index integer NOT NULL,
+// 	    completed boolean NOT NULL,
+// 	    PRIMARY KEY (id),
+// 	    FOREIGN KEY (section)
+// 	        REFERENCES public."Section" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (label)
+// 	        REFERENCES public."Label" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (owner)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."Event"
+// 	(
+// 	    id serial NOT NULL,
+// 	    title character varying(100) NOT NULL,
+// 	    description character varying(1000),
+// 	    "timestampBegin" timestamp with time zone NOT NULL,
+// 	    "timestampEnd" timestamp with time zone,
+// 	    workgroup integer NOT NULL,
+// 	    owner integer NOT NULL,
+// 	    PRIMARY KEY (id),
+// 	    FOREIGN KEY (workgroup)
+// 	        REFERENCES public."WorkGroup" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (owner)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."UserTask"
+// 	(
+// 	    userid integer NOT NULL,
+// 	    task integer NOT NULL,
+// 	    PRIMARY KEY (userid, task),
+// 	    FOREIGN KEY (userid)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (task)
+// 	        REFERENCES public."Task" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."UserEvent"
+// 	(
+// 	    userid integer NOT NULL,
+// 	    event integer NOT NULL,
+// 	    PRIMARY KEY (userid, event),
+// 	    FOREIGN KEY (userid)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (event)
+// 	        REFERENCES public."Event" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."UserWorkGroup"
+// 	(
+// 	    userid integer NOT NULL,
+// 	    workgroup integer NOT NULL,
+// 	    PRIMARY KEY (userid, workgroup),
+// 	    FOREIGN KEY (userid)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (workgroup)
+// 	        REFERENCES public."WorkGroup" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."Document"
+// 	(
+// 	    id serial NOT NULL,
+// 	    name character varying(500) NOT NULL,
+// 	    creationdate timestamp with time zone NOT NULL,
+// 	    isfolder boolean NOT NULL,
+// 	    isnote boolean NOT NULL,
+// 	    path character varying(1000),
+// 	    size integer,
+// 	    owner integer NOT NULL,
+// 	    folder integer,
+// 	    workgroup integer NOT NULL,
+// 	    task integer,
+// 	    PRIMARY KEY (id),
+// 	    UNIQUE (name, folder, isfolder)
+// 	        INCLUDE(name, folder, isfolder),
+// 		FOREIGN KEY (folder)
+// 	        REFERENCES public."Document" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (owner)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (workgroup)
+// 	        REFERENCES public."WorkGroup" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (task)
+// 	        REFERENCES public."Task" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	query = `CREATE TABLE public."UserDocument"
+// 	(
+// 	    userid integer NOT NULL,
+// 	    document integer NOT NULL,
+// 	    PRIMARY KEY (userid, document),
+// 	    FOREIGN KEY (userid)
+// 	        REFERENCES public."User" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID,
+// 	    FOREIGN KEY (document)
+// 	        REFERENCES public."Document" (id) MATCH SIMPLE
+// 	        ON UPDATE NO ACTION
+// 	        ON DELETE NO ACTION
+// 	        NOT VALID
+// 	);`;
+// 	await client.query(query);
+// 	res.sendStatus(200);
+// });
+
+// main.get("/info", async (req, res) => {
+// 	const pool = require("./database");
+// 	const result = await pool.query("select * from information_schema.tables");
+// 	const data = [];
+// 	for (const row of result.rows) data.push(row.table_name);
+// 	res.send({ data });
+// });
+
 const api = require("./api/api");
 main.use("/api", api);
-
-// const admin = require("firebase-admin");
-// const serviceAccount = {
-//   project_id: process.env.PROJECT_ID,
-//   private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-//   client_email: process.env.CLIENT_EMAIL,
-// };
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   storageBucket: process.env.STORAGE,
-// });
-
-// const bucket = admin.storage().bucket();
-
-// const fileUpload = require("express-fileupload");
-
-// main.use(fileUpload());
-
-// main.get("/", (req, res) => {
-//   res.writeHead(200, { "Content-Type": "text/html" });
-//   res.write(
-//     '<form action="upload" method="post" enctype="multipart/form-data">'
-//   );
-//   res.write('<input type="file" name="filetoupload"><br>');
-//   res.write('<input type="submit">');
-//   res.write("</form>");
-//   return res.end();
-// });
-
-// main.post("/upload", async (req, res) => {
-//   for (const name in req.files) {
-//     const file = req.files[name];
-//     const filename = file.name;
-//     const data = file.data;
-//     const fbFile = bucket.file(filename);
-//     await fbFile.save(data);
-//   }
-//   res.send({ msg: "Ok" });
-// });
-
-// main.get("/download", async (req, res) => {
-//   const file = bucket.file("ciao.txt");
-//   const stream = file.createReadStream();
-//   res.attachment("ciao.txt");
-//   stream.pipe(res);
-// });
-
-// main.get("/db", async (req, res) => {
-//   const result = await client.query("select * from ciao");
-//   client.release();
-//   res.status(200).send({ data: result.rows });
-// });
 
 main.listen(process.env.PORT, () => {
 	console.log("Server created");
