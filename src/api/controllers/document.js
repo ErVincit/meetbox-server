53123e1e6faf7e3ac3bcca666edfea447aa5c777
Matteo <orsini.matteo99@gmail.com>
2020-05-22 19:25:28 +0200

    Changed return in get document

--- src/api/controllers/document.js
+++ src/api/controllers/document.js
@@ -4,70 +4,46 @@ const router = express.Router({ mergeParams: true });
 const documentService = require("../services/document");
 
 router.get("/:idDocument", async (req, res, next) => {
-  const { idDocument, idWorkgroup } = req.params;
-  const document = await documentService.get(
-    req.currentUser,
-    idDocument,
-    idWorkgroup
-  );
-  res.status(200).send({ data: document });
+	const { idDocument, idWorkgroup } = req.params;
+	const document = await documentService.get(req.currentUser, idDocument, idWorkgroup);
+	if (document === {}) res.send({ error: "Error", message: "Potresti aver richiesto un documento inesistente o di cui non hai l'accesso" });
+	else res.status(200).send({ data: document });
 });
 
 const request = require("request");
 const url = "http://meetbox.altervista.org/uploads/";
 
 router.get("/:idDocument/download", async (req, res) => {
-  const { idDocument, idWorkgroup } = req.params;
-  const document = await documentService.get(
-    req.currentUser,
-    idDocument,
-    idWorkgroup
-  );
-  if (document.path === "")
-    res.send({ error: "Errore", message: "Path non presente" });
-  else {
-    res.attachment(document.name);
-    request.post(url + document.path).pipe(res);
-  }
+	const { idDocument, idWorkgroup } = req.params;
+	const document = await documentService.get(req.currentUser, idDocument, idWorkgroup);
+	if (document.path === "") res.send({ error: "Errore", message: "Path non presente" });
+	else {
+		res.attachment(document.name);
+		request.post(url + document.path).pipe(res);
+	}
 });
 
 router.put("/:idDocument/edit", async (req, res, next) => {
-  const { idDocument, idWorkgroup } = req.params;
-  const { members, name, folder } = req.body;
-  const currentUser = req.currentUser;
-  try {
-    if (members || name || folder)
-      await documentService.edit(
-        currentUser,
-        idDocument,
-        idWorkgroup,
-        members,
-        name,
-        folder
-      );
-    const data = await documentService.get(
-      currentUser,
-      idDocument,
-      idWorkgroup
-    );
-    return res.send({ data });
-  } catch (err) {
-    return res.send({ error: err.name, message: err.message }).status(500);
-  }
+	const { idDocument, idWorkgroup } = req.params;
+	const { members, name, folder } = req.body;
+	const currentUser = req.currentUser;
+	try {
+		if (members || name || folder) await documentService.edit(currentUser, idDocument, idWorkgroup, members, name, folder);
+		const data = await documentService.get(currentUser, idDocument, idWorkgroup);
+		return res.send({ data });
+	} catch (err) {
+		return res.send({ error: err.name, message: err.message }).status(500);
+	}
 });
 
 router.delete("/:idDocument", async (req, res, next) => {
-  const { idDocument, idWorkgroup } = req.params;
-  try {
-    const data = await documentService.delete(
-      req.currentUser,
-      idDocument,
-      idWorkgroup
-    );
-    res.send({ data });
-  } catch (err) {
-    res.send({ error: err.name, message: err.message });
-  }
+	const { idDocument, idWorkgroup } = req.params;
+	try {
+		const data = await documentService.delete(req.currentUser, idDocument, idWorkgroup);
+		res.send({ data });
+	} catch (err) {
+		res.send({ error: err.name, message: err.message });
+	}
 });
 
 module.exports = router;
